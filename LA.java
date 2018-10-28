092682ca2fbdbada55eca478e8d89dc9e7692210
DESKTOP-LMDRNL3\Ivan <ivan.srbljinovic@outlook.com>
2018-10-28 16:03:31 +0100

    Revert "Citanje iz tablice i incijalizacija liste s Action_E_NKA automatima."
    This reverts commit a75c9555eb0cd271e258cc20f6ba5e9c47caad78.

--- LA.java
+++ LA.java
@@ -7,68 +7,11 @@
  * "Niz uniformnih znakova" ja rezultat rada ovog LA, odnosno to je ono sto se ispisuje na stdin.
  */
 
-import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-
 public class LA {
 
-    private static BufferedReader br;
-    private LinkedList<Action_E_NKA> action_e_nkas;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-    public static void main(String[] args) throws IOException {
-        //Stvaranje ref na datoteku i bf readera
-        File tablica = new File("");
-        br = new BufferedReader(new FileReader(tablica));
-
-
-
-        br.close();
-    }
-
-    private void initializeActEnkas() throws IOException {
-        //Polja potrebna za inicijalizaciju svakog ActionENKA
-        LinkedList<String> action, entry = new LinkedList<>();
-        HashSet<String> allStates, alphabet, acceptableStates = new HashSet<>();
-        HashMap<String, String> function = new HashMap<>();
-        String firstState, name = null;
-        allStates = alphabet = null;
-        firstState = "0";
-        acceptableStates.add("1");
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            if (line.startsWith("<S_")) {
-                name = line;
-                br.readLine();
-                br.readLine();
-            } else if (line.startsWith("{")) {
-                action = extractActions();
-                action_e_nkas.add(new Action_E_NKA(entry, allStates, alphabet, acceptableStates, firstState, function,
-                        name, action));
-            } else {
-                String transitions[] = new String[2];
-                // (5,X->7) Na indeksu nula se nalazi kobinacija stanje,znak, a na indeksu jedan novo stanje
-                transitions = line.split("->");
-                function.put(transitions[0], transitions[1]);
-                allStates.add(transitions[1]);
-                //Na indeksu nula se nalazi trenutno stanje, a na indeksu jedan se nalazi znak
-                transitions = transitions[0].split(",");
-                allStates.add(transitions[0]);
-                alphabet.add(transitions[1]);
-            }
-        }
-    }
-
-
-    private LinkedList<String> extractActions() throws IOException {
-        String line;
-        LinkedList<String> action = new LinkedList<>();
-        while (!((line = br.readLine()).equals("}"))) {
-            action.add(line);
-        }
-        return action;
-    }
+	}
 
 }
