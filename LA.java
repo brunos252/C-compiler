2cf1635cd49ed6c69806a34ba3ecdfb64b4453c1
Ivan Srbljinovic <ivan.srbljinovic@outlook.com>
2018-10-29 12:50:17 +0100

    Ispravljene pogreske u LA.

--- LA.java
+++ LA.java
@@ -15,27 +15,36 @@ import java.util.LinkedList;
 public class LA {
 
     private static BufferedReader br;
-    private LinkedList<Action_E_NKA> action_e_nkas;
+    private static LinkedList<Action_E_NKA> action_e_nkas;
+    private static LinkedList<String> allPosibleStates;
+    private static LinkedList<String> allPosibleActions;
 
     public static void main(String[] args) throws IOException {
         //Stvaranje ref na datoteku i bf readera
-        File tablica = new File("");  //ovdje bi trebalo pisati nesto u stilu \\analizator\\pomocni.txt , ovisno kak ju nazove sacaric
+        File tablica = new File(""); //ovdje bi trebalo pisati nesto u stilu \\analizator\\pomocni.txt , ovisno kak ju nazove sacaric
         br = new BufferedReader(new FileReader(tablica));
         //tu ispod bi islo prvo citanje onih 5 ili kolko redaka i spremanje u odgovarajuče liste, polja itd (to če koristit evacic)
         //a nakon toga citanje cijele datoteke do kraja i to poslat u ovu funkciju kaj si napisal(ako ce funckija sve e-nka inicijalizirat) ili čitanje blokova
         //ako ce funkcija inicijalizirat jedan po jedan
 
+        intializeAllPosibleStates();
+        initalizeAllPosibleActions();
+        initializeActEnkas();
 
         br.close();
 
         //tu nakon inicijalizacije ide evacicev dio, i citas sa stdina (koji je naravno upaljen od pocekta rada ovog programa pa je vazno da u inicijalizaciji
         //se nista ne cita sa stdina)
+
     }
 
-    private void initializeActEnkas() throws IOException { //mislim da ovdje trebas primat u funkciju barem jedan veliki String koji ce u sebi sadrzavat sve za inicijalizaciju jednog enka
-                                                            //mozda ne bi bilo lose da ti je ovo funkicja za inicjalizaciju jednog akcijskog enka, koju pozivas za svakog
-                                                            //jer ako nije onda mislim da je problem  u "else if" dijelu zato kaj za svaki novi akcijski enka moras re-inicijalizirat
-                                                            //sve ove mape,list,i hashove (ili popravi to ili radi funkciju da je za inicijalizaciju jednog akcijskog enka)
+    private static void initializeActEnkas() throws IOException {
+        //mislim da ovdje trebas primat u funkciju barem jedan veliki String koji ce u sebi sadrzavat sve za inicijalizaciju jednog enka
+        //mozda ne bi bilo lose da ti je ovo funkicja za inicjalizaciju jednog akcijskog enka, koju pozivas za svakog
+        //jer ako nije onda mislim da je problem  u "else if" dijelu zato kaj za svaki novi akcijski enka moras re-inicijalizirat
+        //sve ove mape,list,i hashove (ili popravi to ili radi funkciju da je za inicijalizaciju jednog akcijskog enka)
+
+
         //Polja potrebna za inicijalizaciju svakog ActionENKA
         LinkedList<String> action, entry = new LinkedList<>();
         HashSet<String> allStates, alphabet, acceptableStates = new HashSet<>();
@@ -55,6 +64,12 @@ public class LA {
                 action = extractActions();
                 action_e_nkas.add(new Action_E_NKA(entry, allStates, alphabet, acceptableStates, firstState, function,
                         name, action));
+                action.clear();
+                entry.clear();
+                allStates.clear();
+                alphabet.clear();
+                acceptableStates.clear();
+                function.clear();
             } else {
                 String transitions[] = new String[2];
                 // (5,X->7) Na indeksu nula se nalazi kobinacija stanje,znak, a na indeksu jedan novo stanje
@@ -69,14 +84,37 @@ public class LA {
         }
     }
 
-
-    private LinkedList<String> extractActions() throws IOException {
+    private static LinkedList<String> extractActions() throws IOException {
         String line;
         LinkedList<String> action = new LinkedList<>();
         while (!((line = br.readLine()).equals("}"))) {
             action.add(line);
         }
         return action;
+    }
+
+    private static void intializeAllPosibleStates() throws IOException {
+        String line;
+        while ((line = br.readLine()) != null){
+            if (line.startsWith("%X")){
+                String[] states = line.split(" ");
+                for(int i = 1; i < states.length; i++){
+                    allPosibleStates.add(states[i]);
+                }
+            }
+        }
+    }
+
+    private static void initalizeAllPosibleActions() throws IOException {
+        String line;
+        while ((line = br.readLine()) != null){
+            if (line.startsWith("%L")){
+                String[] actions = line.split(" ");
+                for(int i = 1; i < actions.length; i++){
+                    allPosibleActions.add(actions[i]);
+                }
+            }
+        }
     }
 
 }
