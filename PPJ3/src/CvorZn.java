54571f42361fa50f86f7a0c80dae2a650db5ab99
Emanuel Evacic <emanuel.evacic@fer.hr>
2018-12-28 21:22:14 +0000

    Update CvorZn.java

--- PPJ3/src/CvorZn.java
+++ PPJ3/src/CvorZn.java
@@ -1,5 +1,7 @@
 import java.util.LinkedList;
 
+import java.util.LinkedList;
+
 public class CvorZn {
 
         CvorZn roditelj;
@@ -10,18 +12,54 @@ public class CvorZn {
         void dodajDjete(CvorZn cvor){
             this.djeca.add(cvor);
         }
-
+        
+        
+        public CvorZn getroditelj()
+        {
+        	return this.roditelj;
+        }
+        
+        public LinkedList<CvorZn> getdjeca()
+        {
+        	return this.djeca;
+        }
+        
+        public LinkedList<identifikator> getidentifikatori()
+        {
+        	return this.identifikatori;
+        }
+        
         void dodajIdentifikator(identifikator idn){
             this.identifikatori.add(idn);
         }
 
-    private class identifikator{
+    public class identifikator{
         LinkedList<String> tip;
         String ime;
         int DD; //-1: none, 0: deklarirano, 1: definirano
         boolean l_izraz;
         String vrijednost;
-
+        
+        public boolean getl_izraz()
+        {
+        	return this.l_izraz;
+        }
+        
+        public String getime()
+        {
+        	return this.ime;
+        }
+        
+        public LinkedList<String> gettip()
+        {
+        	return this.tip;
+        }
+        
+        public int getDD()
+        {
+        	return this.DD;
+        }
+        
         public identifikator(){
             this.DD = -1;
             this.tip = new LinkedList<>();
@@ -31,3 +69,4 @@ public class CvorZn {
         }
     }
 }
+
