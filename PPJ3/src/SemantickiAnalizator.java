a60492017456b8f5afd5be30766b8b634f4ac207
Emanuel Evacic <emanuel.evacic@fer.hr>
2018-12-28 16:42:59 +0000

    Update SemantickiAnalizator.java

--- PPJ3/src/SemantickiAnalizator.java
+++ PPJ3/src/SemantickiAnalizator.java
@@ -4,13 +4,58 @@ public class SemantickiAnalizator {
 	// write your code here
     }
 
-    private boolean provjeriTip(){
-        //TODO
-        return true;
+     private boolean provjeriTip(LinkedList<String> tip1, LinkedList<String> tip2)
+    {
+    	if(tip1.get(0).equals(tip2.get(0)))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("const_int") & tip2.get(0).equals("int"))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("int")&tip2.get(0).equals("const_int"))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("const_char")&tip2.get(0).equals("char"))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("char")&tip2.get(0).equals("const_char"))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("char")&tip2.get(0).equals("int"))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("niz")&tip1.get(1).equals("int")&tip2.get(0).equals("niz")&tip2.get(1).equals("const_int"))
+    	{
+    		return true;
+    	}
+    	if(tip1.get(0).equals("niz")&tip1.get(1).equals("char")&tip2.get(0).equals("niz")&tip2.get(1).equals("const_char"))
+    	{
+    		return true;
+    	}
+        return false;
     }
 
-    private void greska(Cvor cvor){
-        //TODO
+    private void greska(Cvor cvor)
+    {
+    	System.out.println(cvor.getjedinkaIDN()+"::=");
+    	for (Cvor baby : cvor.getdjeca())
+    	{
+    		if(baby.getjedinkaIDN().equals("<"))
+    		{
+    			System.out.println(baby.getjedinkaIDN());
+    		}
+    		else
+    		{
+    			System.out.println(baby.getjedinkaIDN()+"("+Integer.toString(baby.getjedinkaraz())+","+baby.getjedinkaime()+")");
+    		}
+    	}  	
+        System.exit(0);
     }
 
     private String pretraziTabZnakova(){
