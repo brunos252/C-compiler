b8d77c9153347ad1c7a8938c23f6d28133b45652
Emanuel Evacic <emanuel.evacic@fer.hr>
2018-12-29 20:16:38 +0000

    Update SemantickiAnalizator.java

--- PPJ3/src/SemantickiAnalizator.java
+++ PPJ3/src/SemantickiAnalizator.java
@@ -1,8 +1,8 @@
 import java.util.LinkedList;
 
-import Cvor;
-import CvorZn;
-import CvorZn.identifikator;
+import hehe.Cvor;
+import hehe.CvorZn;
+import hehe.CvorZn.identifikator;
 
 public class SemantickiAnalizator {
 
@@ -10,7 +10,7 @@ public class SemantickiAnalizator {
 	// write your code here
     }
 
-     private boolean provjeriTip(LinkedList<String> tip1, LinkedList<String> tip2)
+    private boolean provjeriTip(LinkedList<String> tip1, LinkedList<String> tip2)
     {
     	if(tip1.get(0).equals(tip2.get(0)))
     	{
@@ -95,7 +95,7 @@ public class SemantickiAnalizator {
     	return null;
     }
 
-  private boolean imaMain(CvorZn korijen_znakova)
+    private boolean imaMain(CvorZn korijen_znakova)
     {
     	boolean ima=false;
     	
@@ -121,7 +121,7 @@ public class SemantickiAnalizator {
         return true;
     }
 
-  private boolean provjeriFun(CvorZn korijen_znakova)
+    private boolean provjeriFun(CvorZn korijen_znakova)
     {
     	for(identifikator IDN:korijen_znakova.getidentifikatori())
     	{
@@ -141,7 +141,79 @@ public class SemantickiAnalizator {
     	return true;
     }
 
-    private void provjeri(){
-        //TODO
+    private void provjeri(Cvor trenutni_cvor, CvorZn trenutni_cvor_u_tablici_znakova)
+    {
+    	if(trenutni_cvor.getjedinkaIDN().equals("<primarni_izraz>"))
+    	{
+    		if(trenutni_cvor.getdjeca().get(0).getjedinkaIDN().equals("IDN"))
+    		{
+    			identifikator IDN=pretraziTabZnakova(trenutni_cvor.getdjeca().get(0),trenutni_cvor_u_tablici_znakova);
+    			if (IDN==null)
+    			{
+    				greska(trenutni_cvor);
+    			}
+    			trenutni_cvor.setTip(IDN.gettip());
+    			trenutni_cvor.setl_izraz(IDN.getl_izraz());	
+    		}
+    		else if(trenutni_cvor.getdjeca().get(0).getjedinkaIDN().equals("BROJ"))
+        	{
+        		if (Integer.parseInt(trenutni_cvor.getdjeca().get(0).getjedinkaime()) < Math.pow(-2, 31) || Integer.parseInt(trenutni_cvor.getdjeca().get(0).getjedinkaime()) > Math.pow(2, 31)-1)
+        		{
+        			greska(trenutni_cvor);
+        		}
+        		LinkedList<String> tip=new LinkedList<>();
+        		tip.add("int");
+        		trenutni_cvor.setTip(tip);
+        		trenutni_cvor.setl_izraz(false);
+        	}
+    		//PROVJERI OVO 
+        	else if(trenutni_cvor.getdjeca().get(0).getjedinkaIDN().equals("ZNAK"))
+        	{
+        		if(!(trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\") || trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\n") || trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\t") || trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\0") || trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\\'") || trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\\"") || trenutni_cvor.getdjeca().get(0).getjedinkaime().startsWith("\\\\")))
+        		{
+        			greska(trenutni_cvor);
+        		}
+        		LinkedList<String> tip=new LinkedList<>();
+        		tip.add("char");
+        		trenutni_cvor.setTip(tip);
+        		trenutni_cvor.setl_izraz(false);
+        	}
+        	else if(trenutni_cvor.getdjeca().get(0).getjedinkaIDN().equals("NIZ ZNAKOVA"))
+        	{
+        		String niz=trenutni_cvor.getdjeca().get(0).getjedinkaime();
+        		int index=niz.indexOf("\\");
+        		while(index>=0)
+        		{
+        			if(!(niz.charAt(index+1)=='n' || niz.charAt(index+1)=='t' || niz.charAt(index+1)=='0' || niz.charAt(index+1)=='\'' || niz.charAt(index+1)=='\"' || niz.charAt(index+1)=='\\'))
+        			{
+        				greska(trenutni_cvor);
+        			}
+        			niz=niz.substring(index+1, niz.length()-1);
+        			index=niz.indexOf("\\");
+        		}
+        		LinkedList<String> tip=new LinkedList<>();
+        		tip.add("niz");
+        		tip.add("const_char");
+        		trenutni_cvor.setTip(tip);
+        		trenutni_cvor.setl_izraz(false);
+        	}
+        	else if(trenutni_cvor.getdjeca().get(0).getjedinkaIDN().equals("L_ZAGRADA"))
+        	{
+        		provjeri(trenutni_cvor.getdjeca().get(1), trenutni_cvor_u_tablici_znakova);
+        		LinkedList<String> pr=trenutni_cvor.getdjeca().get(1).gettip();
+        		trenutni_cvor.setTip(pr);
+        		boolean priv=trenutni_cvor.getdjeca().get(1).getl_izraz();
+        		trenutni_cvor.setl_izraz(priv);
+        	}
+            else
+            {
+                	greska(trenutni_cvor);
+            }
+    	}
+    	
+    	
+    	
+    //kraj metode provjeri	
     }
+//kraj seman
 }
